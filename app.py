bce5fddfcb58b19104720f2abd6afaa708f252ee
Oliver Portee <oliver.portee@gmail.com>
2019-12-26 22:44:56 +0100

    add menu bar, home, blog, contact page

--- app.py
+++ app.py
@@ -1,10 +1,24 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, render_template, url_for
+
+app = Flask(__name__, static_url_path="/static")
+
+
+def website(title=None, content=None, active=None):
+    return render_template("website_template.html", title=title, content=content, active=active)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def home():
+    return website(title="Home@lvrsblc", content="<h1>Home</h1>", active="home")
+
+@app.route('/blog')
+def blog():
+    return website(title="Blog@lvrsblc", content="<h1>Blog</h1>", active="blog")
+
+@app.route('/contact')
+def contact():
+    return website(title="Contact@lvrsblc", content="<h1>Contact</h1>", active="contact")
 
 if __name__ == '__main__':
+    app.debug = True
     app.run()
